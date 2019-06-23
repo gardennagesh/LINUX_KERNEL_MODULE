7a836e185bff3e1283fe07dbd29856a2874513de
Nagesh Nanjundachari <clinuxnagesh@gmail.com>
2019-06-23 21:25:14 +0530

    Update efop.c

--- efop.c
+++ efop.c
@@ -1,6 +1,6 @@
 /*************************************************************************************************************
    Kernel samples by Nagesh N.
-   This module Demo for override ext4 write operation in UBUNTU Kernel 3.13.11.
+   This module Demo for dynamically override ext4 write operation in UBUNTU Kernel 3.13.11.
    I recompiled the kerenel by taking out constant qualifier for file_operations of ext4.
    You could do RW pages, using set_memory_rw. But, I got bug-on-oops while trying on VM over MACBook. 
    And also recent kernels default with CONFIG_STRICT_RWX Which will not allow page attributes change. 
