ea15e1c13714ad3ca3194828eb3764af469cb35e
Nagesh Nanjundachari <clinuxnagesh@gmail.com>
2019-06-24 00:40:38 +0530

    Kernel module to list FileSystems in your Linux.
    Kernel module to list FileSystems in your Linux.

--- fsls.c
+++ fsls.c
@@ -3,7 +3,7 @@
    This module Demo for dynamic listing FS in UBUNTU kernel 3.13.0-32 and 3.13.11.
 
    Module: fsls.c
-   Test Steps: compile, load, dmesg . Chk printk in dmesg and /var/log/system.log
+   Test Steps: compile, insmod, dmesg . Chk printk in dmesg and /var/log/system.log
 *************************************************************************************************************/
 
 #include <linux/module.h>    
